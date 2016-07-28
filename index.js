c4366aa039d9b433ab60f9e9de78f36947d9528b
chadcasady <chad.casady@phtech.com>
2016-07-28 08:44:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ controller.hears('help', ['direct_message', 'direct_mention'], function (bot, me
   bot.reply(message, 'What do you need help with?')
 })
 
+controller.hears('thanks', ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'Don\'t mention it. :thumbsup:')
+})
+
+controller.hears(['eligibility', 'member', 'claim'], ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'Sorry. I don\'t know how to deal with that stuff yet.')
+})
 
 controller.hears(['referral'], 'direct_message', function(bot,message) {
     askReferral = function(response, convo) {
@@ -81,7 +88,7 @@ controller.hears(['referral'], 'direct_message', function(bot,message) {
         {
           pattern: 'email',
           callback: function(response, convo) {
-            convo.say('Ha! I\'m not that smart yet. Here\'s a link instead:');
+            convo.say('Ha! I\'m not that smart yet. Here\'s a fake link instead:');
             reflinkmsg = 'https://cimdemo.phtech.com?referral_id=' + convo.extractResponse('referralID');
             convo.say(reflinkmsg);
             convo.next();
@@ -90,7 +97,7 @@ controller.hears(['referral'], 'direct_message', function(bot,message) {
         {
           pattern: 'link',
           callback: function(response, convo) {
-            convo.say('Here\'s the link to your referral:');
+            convo.say('Here\'s the fake link to your referral:');
             reflinkmsg = 'https://cimdemo.phtech.com?referral_id=' + convo.extractResponse('referralID');
             convo.say(reflinkmsg);
             convo.next();
@@ -99,7 +106,7 @@ controller.hears(['referral'], 'direct_message', function(bot,message) {
         {
           default: true,
           callback: function(response, convo) {
-            convo.say('Here it is anyway:');
+            convo.say('Here\'s the fake link anyway:');
             reflinkmsg = 'https://cimdemo.phtech.com?referral_id=' + convo.extractResponse('referralID');
             convo.say(reflinkmsg);
             convo.next();
@@ -112,13 +119,13 @@ controller.hears(['referral'], 'direct_message', function(bot,message) {
 });
 
 controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+  var text = 'CIM Bot is a silly bot that doesn\'t do much really.'
   var attachments = [{
     fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
+    pretext: 'I ain\'t that smart. :sunglasses: :thumbsup:',
+    title: 'Obligatory link to the PH Tech web site here.',
     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
+    title_link: 'https://phtech.com/',
     text: text,
     color: '#7CD197'
   }]
